fc1b7ee78311420c384a24ae496bea149dc82492
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-09 21:11:24 +0200

    Added debug messages and null guards

--- src/main/bash/stage_smoke.sh
+++ src/main/bash/stage_smoke.sh
@@ -4,8 +4,7 @@ set -e
 
 source pipeline.sh || echo "No pipeline.sh found"
 
-if [[ "${LATEST_PROD_TAG}" == "master" ]]; then
-    echo "No prod release took place - skipping this step"
-else
-    runSmokeTests ${APPLICATION_URL} ${STUBRUNNER_URL}
-fi
+echo "Application URL [${APPLICATION_URL}"
+echo "StubRunner URL [${STUBRUNNER_URL}"
+
+runSmokeTests ${APPLICATION_URL} ${STUBRUNNER_URL}
--- src/main/bash/test_rollback_smoke.sh
+++ src/main/bash/test_rollback_smoke.sh
@@ -4,7 +4,11 @@ set -e
 
 source pipeline.sh || echo "No pipeline.sh found"
 
-if [[ "${LATEST_PROD_TAG}" == "master" ]]; then
+echo "Application URL [${APPLICATION_URL}"
+echo "StubRunner URL [${STUBRUNNER_URL}"
+echo "Latest production tag [${LATEST_PROD_TAG}"
+
+if [[ -z "${LATEST_PROD_TAG}" || "${LATEST_PROD_TAG}" == "master" ]]; then
     echo "No prod release took place - skipping this step"
 else
     runSmokeTests ${APPLICATION_URL} ${STUBRUNNER_URL}
