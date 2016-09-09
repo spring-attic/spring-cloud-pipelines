41aa2df4c61be3fb703202866b93c75819c1463b
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-09 23:12:01 +0200

    Split jobs and views

--- jobs/jenkins_pipeline_sample.groovy
+++ jobs/jenkins_pipeline_sample.groovy
@@ -408,26 +408,4 @@ dsl.job("${projectName}-prod-env-complete") {
 		""")
 	}
 }
-
-dsl.deliveryPipelineView("${projectName}-pipeline") {
-	allowPipelineStart()
-	pipelineInstances(5)
-	showAggregatedPipeline(false)
-	columns(1)
-	updateInterval(5)
-	enableManualTriggers()
-	showAvatars()
-	showChangeLog()
-	pipelines {
-		component("Deployment", "${projectName}-build")
-	}
-	allowRebuild()
-	showDescription()
-	showPromotions()
-	showTotalBuildTime()
-	configure {
-		(it / 'showTestResults').setValue(true)
-		(it / 'pagingEnabled').setValue(true)
-	}
-}
 //  ======= JOBS =======
---
+++ jobs/jenkins_pipeline_sample_view.groovy
@@ -0,0 +1,23 @@
+String projectName = 'jenkins-pipeline-sample'
+
+dsl.deliveryPipelineView("${projectName}-pipeline") {
+	allowPipelineStart()
+	pipelineInstances(5)
+	showAggregatedPipeline(false)
+	columns(1)
+	updateInterval(5)
+	enableManualTriggers()
+	showAvatars()
+	showChangeLog()
+	pipelines {
+		component("Deployment", "${projectName}-build")
+	}
+	allowRebuild()
+	showDescription()
+	showPromotions()
+	showTotalBuildTime()
+	configure {
+		(it / 'showTestResults').setValue(true)
+		(it / 'pagingEnabled').setValue(true)
+	}
+}
