4d471bd7c3a4f59df6d35e747c94db0d96ea6f18
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-09-09 21:23:19 +0200

    Fixed echos

--- src/main/bash/stage_smoke.sh
+++ src/main/bash/stage_smoke.sh
@@ -4,7 +4,7 @@ set -e
 
 source pipeline.sh || echo "No pipeline.sh found"
 
-echo "Application URL [${APPLICATION_URL}"
-echo "StubRunner URL [${STUBRUNNER_URL}"
+echo "Application URL [${APPLICATION_URL}]"
+echo "StubRunner URL [${STUBRUNNER_URL}]"
 
 runSmokeTests ${APPLICATION_URL} ${STUBRUNNER_URL}
--- src/main/bash/test_rollback_smoke.sh
+++ src/main/bash/test_rollback_smoke.sh
@@ -4,9 +4,9 @@ set -e
 
 source pipeline.sh || echo "No pipeline.sh found"
 
-echo "Application URL [${APPLICATION_URL}"
-echo "StubRunner URL [${STUBRUNNER_URL}"
-echo "Latest production tag [${LATEST_PROD_TAG}"
+echo "Application URL [${APPLICATION_URL}]"
+echo "StubRunner URL [${STUBRUNNER_URL}]"
+echo "Latest production tag [${LATEST_PROD_TAG}]"
 
 if [[ -z "${LATEST_PROD_TAG}" || "${LATEST_PROD_TAG}" == "master" ]]; then
     echo "No prod release took place - skipping this step"
