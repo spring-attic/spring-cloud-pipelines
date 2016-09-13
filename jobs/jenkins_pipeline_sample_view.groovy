23902749c07e6584e7121c09ddea220a8d6a565c
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-09-13 15:14:35 +0200

    Fixed views for pipelines

--- jobs/jenkins_pipeline_sample_view.groovy
+++ jobs/jenkins_pipeline_sample_view.groovy
@@ -1,27 +1,28 @@
 import javaposse.jobdsl.dsl.DslFactory
 
-String projectName = 'jenkins-pipeline-sample'
-
 DslFactory dsl = this
 
-dsl.deliveryPipelineView("${projectName}-pipeline") {
-	allowPipelineStart()
-	pipelineInstances(5)
-	showAggregatedPipeline(false)
-	columns(1)
-	updateInterval(5)
-	enableManualTriggers()
-	showAvatars()
-	showChangeLog()
-	pipelines {
-		component("Deployment", "${projectName}-build")
-	}
-	allowRebuild()
-	showDescription()
-	showPromotions()
-	showTotalBuildTime()
-	configure {
-		(it / 'showTestResults').setValue(true)
-		(it / 'pagingEnabled').setValue(true)
+['github-analytics','github-webhook'].each {
+	String projectName = "${it}-pipeline"
+	dsl.deliveryPipelineView(projectName) {
+		allowPipelineStart()
+		pipelineInstances(5)
+		showAggregatedPipeline(false)
+		columns(1)
+		updateInterval(5)
+		enableManualTriggers()
+		showAvatars()
+		showChangeLog()
+		pipelines {
+			component("Deployment", "${projectName}-build")
+		}
+		allowRebuild()
+		showDescription()
+		showPromotions()
+		showTotalBuildTime()
+		configure {
+			(it / 'showTestResults').setValue(true)
+			(it / 'pagingEnabled').setValue(true)
+		}
 	}
 }
