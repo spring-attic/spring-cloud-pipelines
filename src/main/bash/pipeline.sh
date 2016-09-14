cbb4e38abcaaadf172943613a310037c320a78b5
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-09-14 19:36:18 +0200

    Removed the tagger date

--- src/main/bash/pipeline.sh
+++ src/main/bash/pipeline.sh
@@ -218,7 +218,7 @@ function runSmokeTests() {
 }
 
 function findLatestProdTag() {
-    local LAST_PROD_TAG=$(git for-each-ref --sort=taggerdate --format '%(refname) %(taggerdate)' refs/tags/prod | head -n 1)
+    local LAST_PROD_TAG=$(git for-each-ref --sort=taggerdate --format '%(refname)' refs/tags/prod | head -n 1)
     LAST_PROD_TAG=${LAST_PROD_TAG#refs/tags/}
     echo ${LAST_PROD_TAG}
 }
