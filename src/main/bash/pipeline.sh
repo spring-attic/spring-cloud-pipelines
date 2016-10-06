5d3e7c39be98cf109254646573e364966c69e47e
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-10-06 10:13:00 +0200

    Wrapped vars in quotes

--- src/main/bash/pipeline.sh
+++ src/main/bash/pipeline.sh
@@ -3,14 +3,14 @@
 set -e
 
 function logInToCf() {
-    local redownloadInfra=${1}
-    local cfUsername=${2}
-    local cfPassword=${3}
-    local cfOrg=${4}
-    local cfSpace=${5}
-    local apiUrl=${6:-api.run.pivotal.io}
-    CF_INSTALLED=$( cf --version || echo "false" )
-    CF_DOWNLOADED=$( test -r cf && echo "true" || echo "false" )
+    local redownloadInfra="${1}"
+    local cfUsername="${2}"
+    local cfPassword="${3}"
+    local cfOrg="${4}"
+    local cfSpace="${5}"
+    local apiUrl="${6:-api.run.pivotal.io}"
+    CF_INSTALLED="$( cf --version || echo "false" )"
+    CF_DOWNLOADED="$( test -r cf && echo "true" || echo "false" )"
     echo "CF Installed? [${CF_INSTALLED}], CF Downloaded? [${CF_DOWNLOADED}]"
     if [[ ${CF_INSTALLED} == "false" && (${CF_DOWNLOADED} == "false" || ${CF_DOWNLOADED} == "true" && ${redownloadInfra} == "true") ]]; then
         echo "Downloading Cloud Foundry"
@@ -30,136 +30,136 @@ function logInToCf() {
     cf --version
 
     echo "Logging in to CF to org [${cfOrg}], space [${cfSpace}]"
-    cf api --skip-ssl-validation ${apiUrl}
+    cf api --skip-ssl-validation "${apiUrl}"
     set +x
-    cf login -u ${cfUsername} -p ${cfPassword} -o ${cfOrg} -s ${cfSpace}
+    cf login -u "${cfUsername}" -p "${cfPassword}" -o "${cfOrg}" -s "${cfSpace}"
 
 }
 
 function deployRabbitMqToCf() {
-    local rabbitMqAppName=${1:-github-rabbitmq}
+    local rabbitMqAppName="${1:-github-rabbitmq}"
     echo "Waiting for RabbitMQ to start"
     # create RabbitMQ
     APP_NAME="${rabbitMqAppName}"
-    (cf s | grep ${APP_NAME} && echo "found ${APP_NAME}") ||
-        (cf cs cloudamqp lemur ${APP_NAME} && echo "Started RabbitMQ") ||
-        (cf cs p-rabbitmq standard ${APP_NAME}  && echo "Started RabbitMQ for PCF Dev")
+    (cf s | grep "${APP_NAME}" && echo "found ${APP_NAME}") ||
+        (cf cs cloudamqp lemur "${APP_NAME}" && echo "Started RabbitMQ") ||
+        (cf cs p-rabbitmq standard "${APP_NAME}" && echo "Started RabbitMQ for PCF Dev")
 }
 
 function deployAndRestartAppWithName() {
-    local appName=${1}
-    local jarName=${2}
+    local appName="${1}"
+    local jarName="${2}"
     echo "Deploying and restarting app with name [${appName}] and jar name [${jarName}]"
-    deployAppWithName ${appName} ${jarName} 'true'
-    restartApp ${appName}
+    deployAppWithName "${appName}" "${jarName}" 'true'
+    restartApp "${appName}"
 }
 
 function deployAndRestartAppWithNameForSmokeTests() {
-    local appName=${1}
-    local jarName=${2}
+    local appName="${1}"
+    local jarName="${2}"
     local lowerCaseAppName=$( echo "${appName}" | tr '[:upper:]' '[:lower:]' )
     echo "Deploying and restarting app with name [${appName}] and jar name [${jarName}]"
-    deployAppWithName ${appName} ${jarName} 'true'
-    setEnvVar ${lowerCaseAppName} 'spring.profiles.active' "cloud,smoke"
-    restartApp ${appName}
+    deployAppWithName "${appName}" "${jarName}" 'true'
+    setEnvVar "${lowerCaseAppName}" 'spring.profiles.active' "cloud,smoke"
+    restartApp "${appName}"
 }
 
 function appHost() {
-    local appName=${1}
-    local lowerCase=$( echo "${appName}" | tr '[:upper:]' '[:lower:]' )
+    local appName="${1}"
+    local lowerCase="$( echo "${appName}" | tr '[:upper:]' '[:lower:]' )"
     APP_HOST=`cf apps | grep ${lowerCase} | tr -s ' ' | cut -d' ' -f 6 | cut -d, -f1`
     echo "${APP_HOST}"
 }
 
 function deployAppWithName() {
-    local appName=${1}
-    local jarName=${2}
-    local useManifest=${3:-false}
+    local appName="${1}"
+    local jarName="${2}"
+    local useManifest="${3:-false}"
     local manifestOption=$( if [[ "${useManifest}" == "false" ]] ; then echo ""; else echo "--no-manifest" ; fi )
     local lowerCaseAppName=$( echo "${appName}" | tr '[:upper:]' '[:lower:]' )
     echo "Deploying app with name [${lowerCaseAppName}]"
-    cf push ${lowerCaseAppName} -m 1024m -i 1 -p target/${jarName}.jar -n ${lowerCaseAppName} --no-start -b https://github.com/cloudfoundry/java-buildpack.git#v3.8.1 ${manifestOption}
-    APPLICATION_DOMAIN=$( appHost ${lowerCaseAppName} )
+    cf push "${lowerCaseAppName}" -m 1024m -i 1 -p target/${jarName}.jar -n ${lowerCaseAppName} --no-start -b https://github.com/cloudfoundry/java-buildpack.git#v3.8.1 ${manifestOption}
+    APPLICATION_DOMAIN="$( appHost ${lowerCaseAppName} )"
     echo "Determined that application_domain for [${lowerCaseAppName}] is [${APPLICATION_DOMAIN}]"
-    setEnvVar ${lowerCaseAppName} 'APPLICATION_DOMAIN' "${APPLICATION_DOMAIN}"
-    setEnvVar ${lowerCaseAppName} 'JAVA_OPTS' '-Djava.security.egd=file:///dev/urandom'
+    setEnvVar "${lowerCaseAppName}" 'APPLICATION_DOMAIN' "${APPLICATION_DOMAIN}"
+    setEnvVar "${lowerCaseAppName}" 'JAVA_OPTS' '-Djava.security.egd=file:///dev/urandom'
 }
 
 function setEnvVarIfMissing() {
-    local appName=${1}
-    local key=${2}
-    local value=${3}
+    local appName="${1}"
+    local key="${2}"
+    local value="${3}"
     echo "Setting env var [${key}] -> [${value}] for app [${appName}] if missing"
-    cf env ${appName} | grep ${key} || setEnvVar appName key value
+    cf env "${appName}" | grep "${key}" || setEnvVar appName key value
 }
 
 function setEnvVar() {
-    local appName=${1}
-    local key=${2}
-    local value=${3}
+    local appName="${1}"
+    local key="${2}"
+    local value="${3}"
     echo "Setting env var [${key}] -> [${value}] for app [${appName}]"
-    cf set-env ${appName} ${key} ${value}
+    cf set-env "${appName}" "${key}" "${value}"
 }
 
 function restartApp() {
-    local appName=${1}
+    local appName="${1}"
     echo "Restarting app with name [${appName}]"
-    cf restart ${appName}
+    cf restart "${appName}"
 }
 
 function deployEureka() {
-    local redeploy=${1}
-    local jarName=${2}
-    local appName=${3:-github-eureka}
+    local redeploy="${1}"
+    local jarName="${2}"
+    local appName="${3:-github-eureka}"
     echo "Deploying Eureka. Options - redeploy [${redeploy}], jar name [${jarName}], app name [${appName}]"
     if [[ ! -e target/${jarName}.jar || ( -e target/${jarName}.jar && ${redeploy} == "true" ) ]]; then
-        deployAppWithName ${appName} ${jarName}
-        restartApp ${appName}
-        createServiceWithName ${appName}
+        deployAppWithName "${appName}" "${jarName}"
+        restartApp "${appName}"
+        createServiceWithName "${appName}"
     else
         echo "The target/${jarName}.jar is missing or redeploy flag was turned off"
     fi
 }
 
 function deployStubRunnerBoot() {
-    local redeploy=${1}
-    local jarName=${2}
-    local eurekaService=${3:-github-eureka}
-    local rabbitmqService=${4:-github-rabbitmq}
-    local stubRunnerName=${5:-stubrunner}
+    local redeploy="${1}"
+    local jarName="${2}"
+    local eurekaService="${3:-github-eureka}"
+    local rabbitmqService="${4:-github-rabbitmq}"
+    local stubRunnerName="${5:-stubrunner}"
     echo "Deploying Eureka. Options - redeploy [${redeploy}], jar name [${jarName}], app name [${stubRunnerName}], eureka [${eurekaService}], rabbitmq [${rabbitmqService}]"
     if [[ ! -e target/${jarName}.jar || ( -e target/${jarName}.jar && ${redeploy} == "true" ) ]]; then
-        deployAppWithName ${stubRunnerName} ${jarName}
-        local mavenProp=$( extractMavenProperty "stubrunner.ids" )
-        setEnvVar ${stubRunnerName} "stubrunner.ids" "${mavenProp}"
-        bindService ${eurekaService} ${stubRunnerName}
-        bindService ${rabbitmqService} ${stubRunnerName}
-        restartApp ${stubRunnerName}
-        createServiceWithName ${stubRunnerName}
+        deployAppWithName "${stubRunnerName}" "${jarName}"
+        local mavenProp="$( extractMavenProperty "stubrunner.ids" )"
+        setEnvVar "${stubRunnerName}" "stubrunner.ids" "${mavenProp}"
+        bindService "${eurekaService}" "${stubRunnerName}"
+        bindService "${rabbitmqService}" "${stubRunnerName}"
+        restartApp "${stubRunnerName}"
+        createServiceWithName "${stubRunnerName}"
     else
         echo "The target/${jarName}.jar is missing or redeploy flag was turned off"
     fi
 }
 
 function bindService() {
-    local serviceName=${1}
-    local appName=${2}
+    local serviceName="${1}"
+    local appName="${2}"
     echo "Binding service [${serviceName}] to app [${appName}]"
-    cf bind-service ${appName} ${serviceName}
+    cf bind-service "${appName}" "${serviceName}"
 }
 
 function createServiceWithName() {
-    local name=${1}
+    local name="${1}"
     echo "Creating service with name [${name}]"
     APPLICATION_DOMAIN=`cf apps | grep ${name} | tr -s ' ' | cut -d' ' -f 6 | cut -d, -f1`
     JSON='{"uri":"http://'${APPLICATION_DOMAIN}'"}'
-    cf create-user-provided-service ${name} -p ${JSON} || echo "Service already created. Proceeding with the script"
+    cf create-user-provided-service "${name}" -p "${JSON}" || echo "Service already created. Proceeding with the script"
 }
 
 # The function uses Maven Wrapper - if you're using Maven you have to have it on your classpath
 # and change this function
 function extractMavenProperty() {
-    local prop=${1}
+    local prop="${1}"
     MAVEN_PROPERTY=$(./mvnw ${MAVEN_ARGS} -q \
                     -Dexec.executable="echo" \
                     -Dexec.args="\${${prop}}" \
@@ -172,27 +172,27 @@ function extractMavenProperty() {
 
 # The values of group / artifact ids can be later retrieved from Maven
 function downloadJar() {
-    local redownloadInfra=${1}
-    local repoWithJars=${2}
-    local groupId=${3}
-    local artifactId=${4}
-    local version=${5}
-    local destination=target/${artifactId}-${version}.jar
-    local changedGroupId=$( echo "${groupId}" | tr . / )
-    local pathToJar=${repoWithJars}/${changedGroupId}/${artifactId}/${version}/${artifactId}-${version}.jar
+    local redownloadInfra="${1}"
+    local repoWithJars="${2}"
+    local groupId="${3}"
+    local artifactId="${4}"
+    local version="${5}"
+    local destination="target/${artifactId}-${version}.jar"
+    local changedGroupId="$( echo "${groupId}" | tr . / )"
+    local pathToJar="${repoWithJars}/${changedGroupId}/${artifactId}/${version}/${artifactId}-${version}.jar"
     if [[ ! -e ${destination} || ( -e ${destination} && ${redownloadInfra} == "true" ) ]]; then
         mkdir -p target
         echo "Downloading [${pathToJar}] to [${destination}]"
-        curl ${pathToJar} -o ${destination}
+        curl "${pathToJar}" -o "${destination}"
     else
         echo "File [${destination}] exists and redownload flag was set to false. Will not download it again"
     fi
 }
 
 function propagatePropertiesForTests() {
-    local projectArtifactId=${1} 
-    local stubRunnerHost=${2:-stubrunner}
-    local fileLocation=${3:-target/test.properties}
+    local projectArtifactId="${1}"
+    local stubRunnerHost="${2:-stubrunner}"
+    local fileLocation="${3:-target/test.properties}"
     # retrieve host of the app / stubrunner
     # we have to store them in a file that will be picked as properties
     rm -rf target/test.properties
@@ -205,7 +205,7 @@ function propagatePropertiesForTests() {
 }
 
 function readTestPropertiesFromFile() {
-    local fileLocation=${1:-target/test.properties}
+    local fileLocation="${1:-target/test.properties}"
     if [ -f "${fileLocation}" ]
     then
       echo "${fileLocation} found."
@@ -221,34 +221,34 @@ function readTestPropertiesFromFile() {
 
 # Function that executes integration tests
 function runSmokeTests() {
-    local applicationHost=${1}
-    local stubrunnerHost=${2}
-    local version=${3}
+    local applicationHost="${1}"
+    local stubrunnerHost="${2}"
+    local version="${3}"
     if [[ ! -z ${version} ]]; then
         MAVEN_ARGS="${MAVEN_ARGS} -Dversion=${version}"
     fi
     echo "Running smoke tests"
-    ./mvnw clean install -Psmoke -Dapplication.url=${applicationHost} -Dstubrunner.url=${stubrunnerHost} ${MAVEN_ARGS}
+    ./mvnw clean install -Psmoke -Dapplication.url="${applicationHost}" -Dstubrunner.url="${stubrunnerHost}" "${MAVEN_ARGS}"
 }
 
 # Function that executes end to end tests
 function runE2eTests() {
-    local applicationHost=${1}
-    local stubrunnerHost=${2}
+    local applicationHost="${1}"
+    local stubrunnerHost="${2}"
     echo "Running smoke tests"
-    ./mvnw clean install -Pe2e -Dapplication.url=${applicationHost} ${MAVEN_ARGS}
+    ./mvnw clean install -Pe2e -Dapplication.url="${applicationHost}" "${MAVEN_ARGS}"
 }
 
 function findLatestProdTag() {
     local LAST_PROD_TAG=$(git for-each-ref --sort=taggerdate --format '%(refname)' refs/tags/prod | head -n 1)
     LAST_PROD_TAG=${LAST_PROD_TAG#refs/tags/}
-    echo ${LAST_PROD_TAG}
+    echo "${LAST_PROD_TAG}"
 }
 
 function extractVersionFromProdTag() {
-    local tag=${1}
+    local tag="${1}"
     LAST_PROD_VERSION=${tag#prod/}
-    echo ${LAST_PROD_VERSION}
+    echo "${LAST_PROD_VERSION}"
 }
 
 function retrieveGroupId() {
