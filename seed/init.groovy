b42f283772c04b6aa61a5099f13c000d270ee894
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-10-13 08:57:28 +0200

    Added credential creation from code

--- seed/init.groovy
+++ seed/init.groovy
@@ -1,5 +1,7 @@
 import javaposse.jobdsl.dsl.DslScriptLoader
 import javaposse.jobdsl.plugin.JenkinsJobManagement
+import com.cloudbees.plugins.credentials.*
+import com.cloudbees.plugins.credentials.impl.*
 
 def jobScript = new File('/usr/share/jenkins/jenkins_pipeline.groovy')
 def jobManagement = new JenkinsJobManagement(System.out, [:], new File('.'))
@@ -18,4 +20,11 @@ if (m2Created) {
 println "Creating the seed job"
 new DslScriptLoader(jobManagement).with {
 	runScript(jobScript.text)
+}
+
+println "Creating the credentials"
+['cf-test', 'cf-stage', 'cf-prod'].each { String id ->
+	SystemCredentialsProvider.getInstance().getCredentials().add(
+			new UsernamePasswordCredentialsImpl(CredentialsScope.SYSTEM, id, "CF credential [$id]", "user", "pass"));
+	SystemCredentialsProvider.getInstance().save();
 }
