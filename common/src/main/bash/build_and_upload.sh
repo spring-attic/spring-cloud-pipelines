5407b363a32e59eeb72fa86f71b74b454a745e05
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-10-20 09:20:17 +0200

    Added stacktrace to Gradle build

--- common/src/main/bash/build_and_upload.sh
+++ common/src/main/bash/build_and_upload.sh
@@ -8,7 +8,7 @@ if [[ "${PROJECT_TYPE}" == "MAVEN" ]]; then
     ./mvnw versions:set -DnewVersion=${PIPELINE_VERSION} ${MAVEN_ARGS}
     ./mvnw clean verify deploy -Ddistribution.management.release.id=${M2_SETTINGS_REPO_ID} -Ddistribution.management.release.url=${REPO_WITH_JARS} ${MAVEN_ARGS}
 elif [[ "${PROJECT_TYPE}" == "GRADLE" ]]; then
-    ./gradlew clean build deploy -PnewVersion=${PIPELINE_VERSION} -DM2_LOCAL="${M2_LOCAL}" -DREPO_WITH_JARS="${REPO_WITH_JARS}"
+    ./gradlew clean build deploy -PnewVersion=${PIPELINE_VERSION} -DM2_LOCAL="${M2_LOCAL}" -DREPO_WITH_JARS="${REPO_WITH_JARS} --stacktrace"
 else
     echo "Unsupported project build tool"
     return 1
