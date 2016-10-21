b924f78fea583e68a70eee821a27be416147f032
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-10-21 16:51:50 +0200

    WIP

--- concourse/tasks/pipeline.sh
+++ concourse/tasks/pipeline.sh
@@ -20,7 +20,9 @@ export MAVEN_ARGS="-Dmaven.repo.local=${M2_LOCAL}"
 echo "Sourcing file with pipeline functions"
 source ${SCRIPTS_OUTPUT_FOLDER}/pipeline.sh
 
-echo "Setting GRADLE_USER_HOME to tmp"
-export GRADLE_USER_HOME=/tmp
+TEMP_GRADLE_HOME=${ROOT_FOLDER}/${REPO_RESOURCE}/build/gradle_home
+echo "Setting GRADLE_USER_HOME to [${TEMP_GRADLE_HOME}]"
+mkdir -p ${TEMP_GRADLE_HOME}
+export GRADLE_USER_HOME=${TEMP_GRADLE_HOME}
 
 cd ${ROOT_FOLDER}
