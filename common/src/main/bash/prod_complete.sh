0753d58f538cb318cab629eeb4a8b6f6b6165879
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-10-21 16:37:11 +0200

    WIP

--- common/src/main/bash/prod_complete.sh
+++ common/src/main/bash/prod_complete.sh
@@ -4,4 +4,12 @@ set -e
 
 source pipeline.sh || echo "No pipeline.sh found"
 
+export PROJECT_TYPE=$( projectType )
+export OUTPUT_FOLDER=$( outputFolder )
+export TEST_REPORTS_FOLDER=$( testResultsFolder )
+
+echo "Project type [${PROJECT_TYPE}]"
+echo "Output folder [${OUTPUT_FOLDER}]"
+echo "Test reports folder [${TEST_REPORTS_FOLDER}]"
+
 echo 'Disabling blue instance'
--- common/src/main/bash/test_rollback_smoke.sh
+++ common/src/main/bash/test_rollback_smoke.sh
@@ -4,6 +4,14 @@ set -e
 
 source pipeline.sh || echo "No pipeline.sh found"
 
+export PROJECT_TYPE=$( projectType )
+export OUTPUT_FOLDER=$( outputFolder )
+export TEST_REPORTS_FOLDER=$( testResultsFolder )
+
+echo "Project type [${PROJECT_TYPE}]"
+echo "Output folder [${OUTPUT_FOLDER}]"
+echo "Test reports folder [${TEST_REPORTS_FOLDER}]"
+
 echo "Application URL [${APPLICATION_URL}]"
 echo "StubRunner URL [${STUBRUNNER_URL}]"
 echo "Latest production tag [${LATEST_PROD_TAG}]"
