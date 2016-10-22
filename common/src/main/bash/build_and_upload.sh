99954af643c0dc1d0cdf3638b79e840ead23405a
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-10-22 11:10:16 +0200

    WIP

--- common/src/main/bash/build_and_upload.sh
+++ common/src/main/bash/build_and_upload.sh
@@ -6,7 +6,11 @@ source pipeline.sh || echo "No pipeline.sh found"
 
 if [[ "${PROJECT_TYPE}" == "MAVEN" ]]; then
     ./mvnw versions:set -DnewVersion=${PIPELINE_VERSION} ${MAVEN_ARGS}
-    ./mvnw clean verify deploy -Ddistribution.management.release.id=${M2_SETTINGS_REPO_ID} -Ddistribution.management.release.url=${REPO_WITH_JARS} ${MAVEN_ARGS}
+    if [[ "${CI}" == "CONCOURSE" ]]; then
+        ./mvnw clean verify deploy -Ddistribution.management.release.id=${M2_SETTINGS_REPO_ID} -Ddistribution.management.release.url=${REPO_WITH_JARS} ${MAVEN_ARGS} || echo -e "\n\nBuild failed!!! - will print all test results to the console (it's the easiest way to debug anything later)\n\n" && tail -n +1 target/surefire-reports/* && return 1
+    else
+        ./mvnw clean verify deploy -Ddistribution.management.release.id=${M2_SETTINGS_REPO_ID} -Ddistribution.management.release.url=${REPO_WITH_JARS} ${MAVEN_ARGS}
+    fi
 elif [[ "${PROJECT_TYPE}" == "GRADLE" ]]; then
     if [[ "${CI}" == "CONCOURSE" ]]; then
         ./gradlew clean build deploy -PnewVersion=${PIPELINE_VERSION} -DM2_LOCAL="${M2_LOCAL}" -DREPO_WITH_JARS="${REPO_WITH_JARS}" --stacktrace || echo -e "\n\nBuild failed!!! - will print all test results to the console (it's the easiest way to debug anything later)\n\n" && tail -n +1 build/test-results/*.xml && return 1
