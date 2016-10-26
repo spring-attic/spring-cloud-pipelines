8d326801560ff0a861e532c2c88059121b26cfed
Marcin Grzejszczak <marcin@grzejszczak.pl>
2016-10-26 12:35:31 +0200

    WIP

--- jenkins/delete_all_pcfdev_apps.sh
+++ jenkins/delete_all_pcfdev_apps.sh
@@ -4,13 +4,16 @@ cf login -a https://api.local.pcfdev.io --skip-ssl-validation -u admin -p admin 
 
 cf target -o pcfdev-org -s pcfdev-test
 yes | cf delete  github-webhook
+yes | cf delete  github-analytics
 yes | cf delete  github-eureka
 yes | cf delete  stubrunner
 
 cf target -o pcfdev-org -s pcfdev-stage
 yes | cf delete github-webhook
+yes | cf delete  github-analytics
 yes | cf delete github-eureka
 
 cf target -o pcfdev-org -s pcfdev-prod
 yes | cf delete github-webhook
+yes | cf delete  github-analytics
 yes | cf delete github-eureka
