3367dce2596576ddb4aa491acf18985c3e72fb91
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2016-10-26 10:49:22 +0200

    WIP

--- jenkins/kill_all_pcfdev_apps.sh
+++ jenkins/kill_all_pcfdev_apps.sh
@@ -4,13 +4,16 @@ cf login -a https://api.local.pcfdev.io --skip-ssl-validation -u admin -p admin 
 
 cf target -o pcfdev-org -s pcfdev-test
 yes | cf stop github-webhook
+yes | cf stop github-analytics
 yes | cf stop github-eureka
 yes | cf stop stubrunner
 
 cf target -o pcfdev-org -s pcfdev-stage
 yes | cf stop github-webhook
+yes | cf stop github-analytics
 yes | cf stop github-eureka
 
 cf target -o pcfdev-org -s pcfdev-prod
 yes | cf stop github-webhook
+yes | cf stop github-analytics
 yes | cf stop github-eureka
