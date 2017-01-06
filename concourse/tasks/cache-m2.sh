48e3d93aff6545a1bd9510b85774976f4ca5fa90
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-01-06 09:53:29 +0100

    Should fix the m2 docker issue
    fixes #34

--- concourse/tasks/cache-m2.sh
+++ concourse/tasks/cache-m2.sh
@@ -22,7 +22,7 @@ if [ "$1" == "init" ]; then
 fi
 
 cd ${REPO_RESOURCE}
-./mvnw clean verify install ${MAVEN_ARGS}
+./mvnw --fail-never dependency:go-offline ${MAVEN_ARGS} || (./gradlew clean install -PM2_LOCAL=${M2_LOCAL} || echo "Sth went wrong" )
 cd ${ROOT_FOLDER}/m2
 tar -C rootfs -cf rootfs.tar .
 mv rootfs.tar ${ROOT_FOLDER}/to-push/
