dc59d592317e6cb39d048b8d561c4c1fdff6b3b7
Lukasz Szczesny <luk@wybcz.pl>
2017-03-09 21:03:59 +0200

    Remove obsolete create_readme.sh script

--- create_readme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ruby coalesce_readme.rb -i 'docs/README.adoc' -o "README.adoc"
-ruby coalesce_readme.rb -i 'docs/JENKINS.adoc' -o "jenkins/README.adoc"
-ruby coalesce_readme.rb -i 'docs/CONCOURSE.adoc' -o "concourse/README.adoc"
-ruby generate_index.rb
