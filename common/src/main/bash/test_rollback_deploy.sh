f6706c3df86c124d7c13e928cce104861a3940a8
Marcin Grzejszczak <marcin@grzejszczak.pl>
2017-03-23 14:25:42 +0100

    Making services unique only for tests

--- common/src/main/bash/test_rollback_deploy.sh
+++ common/src/main/bash/test_rollback_deploy.sh
@@ -23,7 +23,13 @@ else
     echo "Last prod version equals ${LATEST_PROD_VERSION}"
     downloadJar 'true' ${REPO_WITH_JARS} ${projectGroupId} ${projectArtifactId} ${LATEST_PROD_VERSION}
     logInToCf "${REDOWNLOAD_INFRA}" "${CF_TEST_USERNAME}" "${CF_TEST_PASSWORD}" "${CF_TEST_ORG}" "${CF_TEST_SPACE}" "${CF_TEST_API_URL}"
-    deployAndRestartAppWithNameForSmokeTests ${projectArtifactId} "${projectArtifactId}-${LATEST_PROD_VERSION}"
+    UNIQUE_RABBIT_NAME="rabbitmq-${projectArtifactId}"
+    if [[ "${REDEPLOY_INFRA}" == "true" ]]; then
+        UNIQUE_EUREKA_NAME="eureka-${projectArtifactId}"
+    fi
+    UNIQUE_MYSQL_NAME="mysql-${projectArtifactId}"
+    UNIQUE_STUBRUNNER_NAME="stubrunner-${projectArtifactId}"
+    deployAndRestartAppWithNameForSmokeTests ${projectArtifactId} "${projectArtifactId}-${LATEST_PROD_VERSION}" "${UNIQUE_RABBIT_NAME}" "${UNIQUE_EUREKA_NAME}" "${UNIQUE_MYSQL_NAME}"
     propagatePropertiesForTests ${projectArtifactId}
     # Adding latest prod tag
     echo "LATEST_PROD_TAG=${LATEST_PROD_TAG}" >> ${OUTPUT_FOLDER}/test.properties
