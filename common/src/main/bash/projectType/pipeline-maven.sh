63727aeac0afc26d107ec12f3db8c1c0f4e14520
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-07-18 12:31:02 +0200

    WIP

--- common/src/main/bash/projectType/pipeline-maven.sh
+++ common/src/main/bash/projectType/pipeline-maven.sh
@@ -7,12 +7,11 @@ export MAVEN_OPTS="${MAVEN_OPTS} -Djava.security.egd=file:///dev/urandom"
 function build() {
     echo "Additional Build Options [${BUILD_OPTIONS}]"
 
-    ./mvnw versions:set -DnewVersion=${PIPELINE_VERSION} ${BUILD_OPTIONS}
-    mkdir -p target
+    ./mvnw clean versions:set -DnewVersion=${PIPELINE_VERSION} ${BUILD_OPTIONS}
     if [[ "${CI}" == "CONCOURSE" ]]; then
-        ./mvnw clean verify deploy -Ddistribution.management.release.id="${M2_SETTINGS_REPO_ID}" -Ddistribution.management.release.url="${REPO_WITH_BINARIES}" ${BUILD_OPTIONS} || ( $( printTestResults ) && return 1)
+        ./mvnw verify deploy -Ddistribution.management.release.id="${M2_SETTINGS_REPO_ID}" -Ddistribution.management.release.url="${REPO_WITH_BINARIES}" ${BUILD_OPTIONS} || ( $( printTestResults ) && return 1)
     else
-        ./mvnw clean verify deploy -Ddistribution.management.release.id="${M2_SETTINGS_REPO_ID}" -Ddistribution.management.release.url="${REPO_WITH_BINARIES}" ${BUILD_OPTIONS}
+        ./mvnw verify deploy -Ddistribution.management.release.id="${M2_SETTINGS_REPO_ID}" -Ddistribution.management.release.url="${REPO_WITH_BINARIES}" ${BUILD_OPTIONS}
     fi
 }
 
