c52867e5b39a0f70c219af412a54a03fd9f94c28
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-07-18 12:29:21 +0200

    WIP

--- common/src/main/bash/projectType/pipeline-maven.sh
+++ common/src/main/bash/projectType/pipeline-maven.sh
@@ -8,6 +8,7 @@ function build() {
     echo "Additional Build Options [${BUILD_OPTIONS}]"
 
     ./mvnw versions:set -DnewVersion=${PIPELINE_VERSION} ${BUILD_OPTIONS}
+    mkdir -p target
     if [[ "${CI}" == "CONCOURSE" ]]; then
         ./mvnw clean verify deploy -Ddistribution.management.release.id="${M2_SETTINGS_REPO_ID}" -Ddistribution.management.release.url="${REPO_WITH_BINARIES}" ${BUILD_OPTIONS} || ( $( printTestResults ) && return 1)
     else
