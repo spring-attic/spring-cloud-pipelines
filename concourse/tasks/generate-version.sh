fcb6cad1204c98641cfd2010568838ec7fe195b3
Ben Hale <nebhale@nebhale.com>
2017-07-28 08:15:29 -0700

    Improve Concourse (#70)
    * Concourse 3.2 Syntax
    
    This change updates the pipeline definition to use the proper substitution
    syntax for Concourse 3.2 and later.
    
    * YAML Reference
    
    This change updates the pipeline to make use of YAML references.  This allows
    common blocks of code (like parameter groups) to be defined once and shared
    between jobs.
    
    * Formatting Consistency
    
    This change modifies the format a bit to make it consistent across the entire
    file.  This mostly has to do with the indention of YAML arrays.
    
    * Improve Generate Version
    
    The change improves the generate-version task. It does this by using a more
    descriptive output name, using a local clone as recommended by the Concourse
    team, and only configuring the local git repository.

--- concourse/tasks/generate-version.sh
+++ concourse/tasks/generate-version.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+set -e -u
+
 VERSION=1.0.0.M1-`date +%Y%m%d_%H%M%S`-VERSION
 MESSAGE="[Concourse CI] Bump to Next Version ($VERSION)"
 
-cd out
+git clone version updated-version
+pushd updated-version
+  git config --local user.email "${GIT_EMAIL}"
+  git config --local user.name "${GIT_NAME}"
 
-cp -r ../version/. ./
-echo "Bump to ${VERSION}"
-echo "${VERSION}" > version
+  echo "Bump to ${VERSION}"
+  echo "${VERSION}" > version
 
-git config --global user.email "${GIT_EMAIL}"
-git config --global user.name "${GIT_NAME}"
-git add version
-git commit -m "${MESSAGE}"
+  git add version
+  git commit -m "${MESSAGE}"
+popd
