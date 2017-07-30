4d81ad98060215b88fa791e585cd88a6919b15e5
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-07-30 08:14:46 +0200

    WIP

--- common/src/main/bash/projectType/pipeline-maven.sh
+++ common/src/main/bash/projectType/pipeline-maven.sh
@@ -3,7 +3,7 @@ set -e
 
 # It takes ages on Docker to run the app without this
 if [[ ${BUILD_OPTIONS} != *"java.security.egd"* ]]; then
-    if [[ ! -z ${BUILD_OPTIONS} || ${BUILD_OPTIONS} != "null" ]]; then
+    if [[ ! -z ${BUILD_OPTIONS} || ${BUILD_OPTIONS} != *"null"* ]]; then
         export BUILD_OPTIONS="${BUILD_OPTIONS} -Djava.security.egd=file:///dev/urandom"
     else
         export BUILD_OPTIONS="-Djava.security.egd=file:///dev/urandom"
