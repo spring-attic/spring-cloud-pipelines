ab90912501d5ad4ef3f206adf0dc03a04d1d9f2c
Lukasz Szczesny <luk@wybcz.pl>
2017-09-08 00:22:00 +0200

    Simplify deployServices() function

--- common/src/main/bash/pipeline.sh
+++ common/src/main/bash/pipeline.sh
@@ -136,31 +136,28 @@ function serviceExists() {
 # For other environments only deploys a service if it wasn't there.
 # Uses ruby and jq
 function deployServices() {
-  if [[ "$( pipelineDescriptorExists )" == "true" ]]; then
-    export PARSED_YAML=$( yaml2json "pipeline.yml" )
-    while read -r line; do
-      for service in "${line}"
-      do
-        set ${service}
-        serviceType=${1}
-        serviceName=${2}
-        serviceCoordinates=${3}
-        if [[ "${ENVIRONMENT}" == "TEST" ]]; then
-          deleteService "${serviceType}" "${serviceName}"
-          deployService "${serviceType}" "${serviceName}" "${serviceCoordinates}"
-        else
-          if [[ "$( serviceExists ${serviceName} )" == "true" ]]; then
-            echo "Skipping deployment since service is already deployed"
-          else
-            deployService "${serviceType}" "${serviceName}" "${serviceCoordinates}"
-          fi
-        fi
-      done
-    # Removes quotes from the result and retrieve the space separated type, name and coordinates
-    done <<< "$( echo "${PARSED_YAML}" | jq --arg x ${LOWER_CASE_ENV} '.[$x].services[] | "\(.type) \(.name) \(.coordinates)"' | sed 's/^"\(.*\)"$/\1/' )"
-  else
+  if [[ "$( pipelineDescriptorExists )" != "true" ]]; then
     echo "No pipeline descriptor found - will not deploy any services"
+    return
   fi
+
+  PARSED_YAML=$( yaml2json "pipeline.yml" )
+  export PARSED_YAML
+
+  while read -r serviceType serviceName serviceCoordinates; do
+    if [[ "${ENVIRONMENT}" == "TEST" ]]; then
+      deleteService "${serviceType}" "${serviceName}"
+      deployService "${serviceType}" "${serviceName}" "${serviceCoordinates}"
+    else
+      if [[ "$( serviceExists "${serviceName}" )" == "true" ]]; then
+        echo "Skipping deployment since service is already deployed"
+      else
+        deployService "${serviceType}" "${serviceName}" "${serviceCoordinates}"
+      fi
+    fi
+  # retrieve the space separated type, name and coordinates
+  done <<< "$( echo "${PARSED_YAML}" | \
+               jq -r --arg x "${LOWER_CASE_ENV}" '.[$x].services[] | "\(.type) \(.name) \(.coordinates)"' )"
 }
 
 # Converts YAML to JSON - uses ruby
