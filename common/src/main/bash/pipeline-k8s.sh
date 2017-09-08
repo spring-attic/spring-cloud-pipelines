eead84986e41775fb7cfb027d650cb43683ebf2d
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-09-08 11:21:28 +0200

    Checking health endpoint

--- common/src/main/bash/pipeline-k8s.sh
+++ common/src/main/bash/pipeline-k8s.sh
@@ -431,14 +431,10 @@ function isAppRunning() {
     local waitTime=5
     local retries=50
     local running=1
-    # TODO: Why the hell I can't access /health on some services but can access other endpoints????
-    local primaryHealthEndpoint="health"
-    local secondaryHealthEndpoint="info"
+    local healthEndpoint="health"
     for i in $( seq 1 "${retries}" ); do
         sleep "${waitTime}"
-        # TODO: Adding secondary health endpoint cause for some reason sometimes you can't access /health
-        curl -m 5 "${host}:${port}/${primaryHealthEndpoint}" && running=0 && break ||
-            curl -m 5 "${host}:${port}/${secondaryHealthEndpoint}" && running=0 && break
+        curl -m 5 "${host}:${port}/${healthEndpoint}" && running=0 && break
         echo "Fail #$i/${retries}... will try again in [${waitTime}] seconds"
     done
     if [[ "${running}" == 1 ]]; then
