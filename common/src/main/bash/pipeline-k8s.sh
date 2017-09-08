35d552323135996103422f0f08bb47662be1593e
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-09-08 14:12:58 +0200

    Added logging for blue / green

--- common/src/main/bash/pipeline-k8s.sh
+++ common/src/main/bash/pipeline-k8s.sh
@@ -579,7 +579,7 @@ function deleteBlueInstance() {
     # Log in to CF to start deployment
     logInToPaas
     # find the oldest version and remove it
-    local oldestDeployment="$( oldestDeployment )"
+    local oldestDeployment="$( oldestDeployment "${appName}" )"
     if [[ "${oldestDeployment}" != "" ]]; then
         echo "Deleting deployment with name [${oldestDeployment}]"
         kubectl --context="${K8S_CONTEXT}" --namespace="${PAAS_NAMESPACE}" delete deployment "${oldestDeployment}"
@@ -591,7 +591,7 @@ function deleteBlueInstance() {
 function oldestDeployment() {
     local appName="${1}"
     local changedAppName="$( escapeValueForDns ${appName}-${PIPELINE_VERSION} )"
-    local deployedApps="$( kubectl --context="${K8S_CONTEXT}" --namespace="${PAAS_NAMESPACE}" deployments -lname="${appName}" --no-headers | awk '{print $1}' |  grep -v "${changedAppName}" )"
+    local deployedApps="$( kubectl --context="${K8S_CONTEXT}" --namespace="${PAAS_NAMESPACE}" deployments -lname="${appName}" --no-headers | awk '{print $1}' | grep -v "${changedAppName}" )"
     local oldestDeployment="$( echo "${deployedApps}" | sort | head -n 1 )"
     echo "${oldestDeployment}"
 }
