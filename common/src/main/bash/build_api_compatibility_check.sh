08b083e44882220492c37ef87214234583341c81
Lukasz Szczesny <luk@wybcz.pl>
2017-09-08 11:32:38 +0200

    Do not check sourced pipeline.sh

--- common/src/main/bash/build_api_compatibility_check.sh
+++ common/src/main/bash/build_api_compatibility_check.sh
@@ -6,6 +6,7 @@ __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 export ENVIRONMENT=BUILD
 
+# shellcheck source=/dev/null
 [[ -f "${__DIR}/pipeline.sh" ]] && source "${__DIR}/pipeline.sh" || \
     echo "No pipeline.sh found"
 
