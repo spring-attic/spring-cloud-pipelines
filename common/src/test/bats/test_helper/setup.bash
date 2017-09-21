69e4d3ff961be13e6b24927d49e756d57e72d779
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-09-21 16:16:02 +0200

    Trying to make tests pass

--- common/src/test/bats/test_helper/setup.bash
+++ common/src/test/bats/test_helper/setup.bash
@@ -2,6 +2,6 @@
 export PIPELINES_TEST_DIR="${BATS_TMPDIR}/BATS"
 rm -rf "${PIPELINES_TEST_DIR}"
 mkdir -p "${PIPELINES_TEST_DIR}"
-cp -R "${BATS_TEST_DIRNAME}/../../main/bash/" "${PIPELINES_TEST_DIR}"
-cp -R "${BATS_TEST_DIRNAME}/fixtures/" "${PIPELINES_TEST_DIR}"
+cp -rf "${BATS_TEST_DIRNAME}/../../main/bash/" "${PIPELINES_TEST_DIR}"
+cp -rf "${BATS_TEST_DIRNAME}/fixtures/" "${PIPELINES_TEST_DIR}"
 cd "${PIPELINES_TEST_DIR}"
