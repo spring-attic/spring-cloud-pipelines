a2d40af6ceb8f43fb72be354a7aa34f93f1b682a
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-09-21 16:24:03 +0200

    Trying to make tests pass

--- common/src/test/bats/test_helper/setup.bash
+++ common/src/test/bats/test_helper/setup.bash
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+# https://unix.stackexchange.com/questions/228597/how-to-copy-a-folder-recursively-in-an-idempotent-way-using-cp
+shopt -s dotglob
+
 export PIPELINES_TEST_DIR="${BATS_TMPDIR}/BATS"
 rm -rf "${PIPELINES_TEST_DIR}"
 mkdir -p "${PIPELINES_TEST_DIR}"
-cp -rf "${BATS_TEST_DIRNAME}/../../main/bash/" "${PIPELINES_TEST_DIR}"
-cp -rf "${BATS_TEST_DIRNAME}/fixtures/" "${PIPELINES_TEST_DIR}"
+cp -R "${BATS_TEST_DIRNAME}"/../../main/bash/* "${PIPELINES_TEST_DIR}/"
+cp -R "${BATS_TEST_DIRNAME}"/fixtures/* "${PIPELINES_TEST_DIR}/"
 cd "${PIPELINES_TEST_DIR}"
