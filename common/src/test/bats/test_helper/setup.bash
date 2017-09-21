86ad1b7ca9d1690cf453dac09af52c7c8c61b229
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-09-21 16:12:20 +0200

    Added missing parent folder generation

--- common/src/test/bats/test_helper/setup.bash
+++ common/src/test/bats/test_helper/setup.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 export PIPELINES_TEST_DIR="${BATS_TMPDIR}/BATS"
 rm -rf "${PIPELINES_TEST_DIR}"
+mkdir -p "${PIPELINES_TEST_DIR}"
 cp -R "${BATS_TEST_DIRNAME}/../../main/bash/" "${PIPELINES_TEST_DIR}"
 cp -R "${BATS_TEST_DIRNAME}/fixtures/" "${PIPELINES_TEST_DIR}"
 cd "${PIPELINES_TEST_DIR}"
