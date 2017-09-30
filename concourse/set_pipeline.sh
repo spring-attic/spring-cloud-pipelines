c764db59fef70c89b1e0074a5c342a62880a36fb
Mark Alston <malston@pivotal.io>
2017-09-30 02:06:50 -0600

    Refactor Concourse folder structure (#91)

--- concourse/set_pipeline.sh
+++ concourse/set_pipeline.sh
@@ -4,4 +4,4 @@ PIPELINE_NAME=${1:-github-webhook}
 ALIAS=${2:-docker}
 CREDENTIALS=${3:-credentials.yml}
 
-echo y | fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "${CREDENTIALS}"
+fly -t "${ALIAS}" sp -p "${PIPELINE_NAME}" -c pipeline.yml -l "${CREDENTIALS}" -n
