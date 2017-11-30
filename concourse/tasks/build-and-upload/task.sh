98af80800d5908fda1286109c5f051792f89560e
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-11-30 12:53:08 +0100

    ;(

--- concourse/tasks/build-and-upload/task.sh
+++ concourse/tasks/build-and-upload/task.sh
@@ -18,15 +18,14 @@ source /docker-lib.sh || echo "Failed to source docker-lib.sh... Hopefully you k
 start_docker || echo "Failed to start docker... Hopefully you know what you're doing"
 
 # shellcheck source=/dev/null
-source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/concourse/tasks/pipeline.sh"
+source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/concourse/tasks/pipeline.sh" || echo "Failed"
 
 echo "Building and uploading the projects artifacts"
 cd "${ROOT_FOLDER}"/"${REPO_RESOURCE}" || exit
 
 # shellcheck source=/dev/null
-. "${SCRIPTS_OUTPUT_FOLDER}/build_and_upload.sh"
+. "${SCRIPTS_OUTPUT_FOLDER}/build_and_upload.sh" || echo "Failed"
 
 echo "Tagging the project with dev tag"
-echo "dev/${PIPELINE_VERSION}" > "${ROOT_FOLDER}/${REPO_RESOURCE}/tag"
-cp -r "${ROOT_FOLDER}/${REPO_RESOURCE}"/. "${ROOT_FOLDER}/${OUTPUT_RESOURCE}/"
-exit 0
+echo "dev/${PIPELINE_VERSION}" > "${ROOT_FOLDER}/${REPO_RESOURCE}/tag" || echo "Failed"
+cp -r "${ROOT_FOLDER}/${REPO_RESOURCE}"/. "${ROOT_FOLDER}/${OUTPUT_RESOURCE}/" || echo "Failed"
