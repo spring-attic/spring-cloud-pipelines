3a3be9a5cdf1473109f8935ad6da825bc668b28e
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-11-30 15:06:50 +0100

    FOO2

--- concourse/tasks/pipeline.sh
+++ concourse/tasks/pipeline.sh
@@ -6,7 +6,7 @@ echo "Scripts will be copied to [${SCRIPTS_OUTPUT_FOLDER}]"
 echo "Copying pipelines scripts"
 cd "${ROOT_FOLDER}/${REPO_RESOURCE}" || exit
 mkdir -p "${SCRIPTS_OUTPUT_FOLDER}" || echo "Failed to create the scripts output folder"
-cp -r "${ROOT_FOLDER}/${TOOLS_RESOURCE}"/common/src/main/bash/* "${SCRIPTS_OUTPUT_FOLDER}"/
+cp -r "${ROOT_FOLDER}/${TOOLS_RESOURCE}common/src/main/bash/". "${SCRIPTS_OUTPUT_FOLDER}"/
 [[ -d "${ROOT_FOLDER}/${CUSTOM_SCRIPT_IDENTIFIER}" ]] && \
     cp -r "${ROOT_FOLDER}/${CUSTOM_SCRIPT_IDENTIFIER}"/common/src/main/bash/* "${SCRIPTS_OUTPUT_FOLDER}"/ || \
     echo "No custom scripts found"
