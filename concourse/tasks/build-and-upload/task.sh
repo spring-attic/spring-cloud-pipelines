1fa0de8a3533b4a3538f81ba9188306bc58ef9ed
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-11-30 13:23:46 +0100

    ;( ;(

--- concourse/tasks/build-and-upload/task.sh
+++ concourse/tasks/build-and-upload/task.sh
@@ -12,11 +12,11 @@ echo "Repo resource folder is [${REPO_RESOURCE}]"
 echo "Tools resource folder is [${TOOLS_RESOURCE}]"
 echo "Version resource folder is [${VERSION_RESOURCE}]"
 
-## If you're using some other image with Docker change these lines
-## shellcheck source=/dev/null
-#source /docker-lib.sh || echo "Failed to source docker-lib.sh... Hopefully you know what you're doing"
-#start_docker || echo "Failed to start docker... Hopefully you know what you're doing"
-#
+# If you're using some other image with Docker change these lines
+# shellcheck source=/dev/null
+source /docker-lib.sh || echo "Failed to source docker-lib.sh... Hopefully you know what you're doing"
+start_docker || echo "Failed to start docker... Hopefully you know what you're doing"
+
 # shellcheck source=/dev/null
 source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/concourse/tasks/pipeline.sh" || echo "Failed"
 #
