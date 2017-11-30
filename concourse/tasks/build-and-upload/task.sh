f34d8fc83dd1a08a56c90aa89e281317f2b8d6f6
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-11-30 13:15:31 +0100

    ;( ;(

--- concourse/tasks/build-and-upload/task.sh
+++ concourse/tasks/build-and-upload/task.sh
@@ -12,18 +12,18 @@ echo "Repo resource folder is [${REPO_RESOURCE}]"
 echo "Tools resource folder is [${TOOLS_RESOURCE}]"
 echo "Version resource folder is [${VERSION_RESOURCE}]"
 
-# If you're using some other image with Docker change these lines
-# shellcheck source=/dev/null
-source /docker-lib.sh || echo "Failed to source docker-lib.sh... Hopefully you know what you're doing"
-start_docker || echo "Failed to start docker... Hopefully you know what you're doing"
-
-# shellcheck source=/dev/null
-source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/concourse/tasks/pipeline.sh" || echo "Failed"
-
-echo "Building and uploading the projects artifacts"
-cd "${ROOT_FOLDER}"/"${REPO_RESOURCE}" || exit
-
-# shellcheck source=/dev/null
+## If you're using some other image with Docker change these lines
+## shellcheck source=/dev/null
+#source /docker-lib.sh || echo "Failed to source docker-lib.sh... Hopefully you know what you're doing"
+#start_docker || echo "Failed to start docker... Hopefully you know what you're doing"
+#
+## shellcheck source=/dev/null
+#source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/concourse/tasks/pipeline.sh" || echo "Failed"
+#
+#echo "Building and uploading the projects artifacts"
+#cd "${ROOT_FOLDER}"/"${REPO_RESOURCE}" || exit
+#
+## shellcheck source=/dev/null
 #. "${SCRIPTS_OUTPUT_FOLDER}/build_and_upload.sh" || echo "Failed"
 
 echo "Tagging the project with dev tag"
