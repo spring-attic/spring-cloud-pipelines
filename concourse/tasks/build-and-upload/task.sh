a3187a756e003f06893cdc5858a0cada0157dfb6
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-11-30 16:03:48 +0100

    debug

--- concourse/tasks/build-and-upload/task.sh
+++ concourse/tasks/build-and-upload/task.sh
@@ -26,6 +26,6 @@ source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/concourse/tasks/pipeline.sh"
 ## shellcheck source=/dev/null
 #. "${SCRIPTS_OUTPUT_FOLDER}/build_and_upload.sh"
 #
-#echo "Tagging the project with dev tag"
-#echo "dev/${PIPELINE_VERSION}" > "${ROOT_FOLDER}/${REPO_RESOURCE}/tag"
-#cp -r "${ROOT_FOLDER}/${REPO_RESOURCE}"/. "${ROOT_FOLDER}/${OUTPUT_RESOURCE}/"
+echo "Tagging the project with dev tag"
+echo "dev/${PIPELINE_VERSION}" > "${ROOT_FOLDER}/${REPO_RESOURCE}/tag"
+cp -r "${ROOT_FOLDER}/${REPO_RESOURCE}"/. "${ROOT_FOLDER}/${OUTPUT_RESOURCE}/"
