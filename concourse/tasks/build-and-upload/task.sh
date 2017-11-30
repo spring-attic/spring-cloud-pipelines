296e5ffc3a0c1131ca935bccb4e352ad8e2b4d55
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-11-30 13:31:17 +0100

    ;(

--- concourse/tasks/build-and-upload/task.sh
+++ concourse/tasks/build-and-upload/task.sh
@@ -21,7 +21,7 @@ start_docker || echo "Failed to start docker... Hopefully you know what you're d
 source "${ROOT_FOLDER}/${TOOLS_RESOURCE}/concourse/tasks/pipeline.sh"
 
 echo "Building and uploading the projects artifacts"
-cd "${ROOT_FOLDER}"/"${REPO_RESOURCE}" || exit
+cd "${ROOT_FOLDER}/${REPO_RESOURCE}" || exit
 
 # shellcheck source=/dev/null
 . "${SCRIPTS_OUTPUT_FOLDER}/build_and_upload.sh"
