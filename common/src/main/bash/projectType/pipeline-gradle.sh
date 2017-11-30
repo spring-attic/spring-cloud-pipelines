aa7d9fd7127738f78eababa5ead3a101b67efde3
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2017-11-30 14:05:17 +0100

    ;(

--- common/src/main/bash/projectType/pipeline-gradle.sh
+++ common/src/main/bash/projectType/pipeline-gradle.sh
@@ -16,13 +16,9 @@ fi
 
 function build() {
 	BUILD_OPTIONS="${BUILD_OPTIONS} -DM2_SETTINGS_REPO_USERNAME=${M2_SETTINGS_REPO_USERNAME} -DM2_SETTINGS_REPO_PASSWORD=${M2_SETTINGS_REPO_PASSWORD}"
-	if [[ "${CI}" == "CONCOURSE" ]]; then
-		# shellcheck disable=SC2086
-		"${GRADLEW_BIN}" clean build deploy -PnewVersion="${PIPELINE_VERSION}" -DREPO_WITH_BINARIES="${REPO_WITH_BINARIES}" -DREPO_WITH_BINARIES_FOR_UPLOAD="${REPO_WITH_BINARIES_FOR_UPLOAD}" --stacktrace ${BUILD_OPTIONS} || (printTestResults && return 1)
-	else
-		# shellcheck disable=SC2086
-		"${GRADLEW_BIN}" clean build deploy -PnewVersion="${PIPELINE_VERSION}" -DREPO_WITH_BINARIES="${REPO_WITH_BINARIES}" -DREPO_WITH_BINARIES_FOR_UPLOAD="${REPO_WITH_BINARIES_FOR_UPLOAD}" --stacktrace ${BUILD_OPTIONS} || (echo "Build failed!!!" && return 1)
-	fi
+	# shellcheck disable=SC2086
+	"${GRADLEW_BIN}" clean build deploy -PnewVersion="${PIPELINE_VERSION}" -DREPO_WITH_BINARIES="${REPO_WITH_BINARIES}" -DREPO_WITH_BINARIES_FOR_UPLOAD="${REPO_WITH_BINARIES_FOR_UPLOAD}" --stacktrace ${BUILD_OPTIONS}
+	echo "foo $?"
 }
 
 function apiCompatibilityCheck() {
@@ -36,13 +32,8 @@ function apiCompatibilityCheck() {
 		# Downloading latest jar
 		LATEST_PROD_VERSION=${LATEST_PROD_TAG#prod/}
 		echo "Last prod version equals [${LATEST_PROD_VERSION}]"
-		if [[ "${CI}" == "CONCOURSE" ]]; then
-			# shellcheck disable=SC2086
-			"${GRADLEW_BIN}" clean apiCompatibility -DlatestProductionVersion="${LATEST_PROD_VERSION}" -DREPO_WITH_BINARIES="${REPO_WITH_BINARIES_FOR_UPLOAD}" --stacktrace ${BUILD_OPTIONS} || (printTestResults && return 1)
-		else
-			# shellcheck disable=SC2086
-			"${GRADLEW_BIN}" clean apiCompatibility -DlatestProductionVersion="${LATEST_PROD_VERSION}" -DREPO_WITH_BINARIES="${REPO_WITH_BINARIES_FOR_UPLOAD}" --stacktrace ${BUILD_OPTIONS}
-		fi
+		# shellcheck disable=SC2086
+		"${GRADLEW_BIN}" clean apiCompatibility -DlatestProductionVersion="${LATEST_PROD_VERSION}" -DREPO_WITH_BINARIES="${REPO_WITH_BINARIES_FOR_UPLOAD}" --stacktrace ${BUILD_OPTIONS}
 	fi
 }
 
@@ -54,11 +45,6 @@ function retrieveAppName() {
 	"${GRADLEW_BIN}" artifactId -q | tail -1
 }
 
-function printTestResults() {
-	# shellcheck disable=SC1117
-	echo -e "\n\nBuild failed!!! - will print all test results to the console (it's the easiest way to debug anything later)\n\n" && tail -n +1 "$(testResultsAntPattern)"
-}
-
 function retrieveStubRunnerIds() {
 	"${GRADLEW_BIN}" stubIds -q | tail -1
 }
@@ -68,34 +54,19 @@ function runSmokeTests() {
 	local stubrunnerUrl="${STUBRUNNER_URL}"
 	echo "Running smoke tests. Application url [${applicationUrl}], Stubrunner Url [${stubrunnerUrl}]"
 
-	if [[ "${CI}" == "CONCOURSE" ]]; then
-		# shellcheck disable=SC2086
-		"${GRADLEW_BIN}" smoke -PnewVersion="${PIPELINE_VERSION}" -Dapplication.url="${applicationUrl}" -Dstubrunner.url="${stubrunnerUrl}" ${BUILD_OPTIONS} || (printTestResults && return 1)
-	else
-		# shellcheck disable=SC2086
-		"${GRADLEW_BIN}" smoke -PnewVersion="${PIPELINE_VERSION}" -Dapplication.url="${applicationUrl}" -Dstubrunner.url="${stubrunnerUrl}" ${BUILD_OPTIONS}
-	fi
+	# shellcheck disable=SC2086
+	"${GRADLEW_BIN}" smoke -PnewVersion="${PIPELINE_VERSION}" -Dapplication.url="${applicationUrl}" -Dstubrunner.url="${stubrunnerUrl}" ${BUILD_OPTIONS}
 }
 
 function runE2eTests() {
 	local applicationUrl="${APPLICATION_URL}"
 	echo "Running e2e tests for application with url [${applicationUrl}]"
-
-	if [[ "${CI}" == "CONCOURSE" ]]; then
-		# shellcheck disable=SC2086
-		"${GRADLEW_BIN}" e2e -PnewVersion="${PIPELINE_VERSION}" -Dapplication.url="${applicationUrl}" ${BUILD_OPTIONS} || (printTestResults && return 1)
-	else
-		# shellcheck disable=SC2086
-		"${GRADLEW_BIN}" e2e -PnewVersion="${PIPELINE_VERSION}" -Dapplication.url="${applicationUrl}" ${BUILD_OPTIONS}
-	fi
+	# shellcheck disable=SC2086
+	"${GRADLEW_BIN}" e2e -PnewVersion="${PIPELINE_VERSION}" -Dapplication.url="${applicationUrl}" ${BUILD_OPTIONS}
 }
 
 function outputFolder() {
 	echo "build/libs"
-}
-
-function testResultsAntPattern() {
-	echo "**/test-results/*.xml"
 }
 
 export -f build
@@ -103,4 +74,3 @@ export -f apiCompatibilityCheck
 export -f runSmokeTests
 export -f runE2eTests
 export -f outputFolder
-export -f testResultsAntPattern
