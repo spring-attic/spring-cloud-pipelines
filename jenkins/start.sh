b134b51f6a9e21f288640080dc0080c329454e8d
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2018-02-28 09:34:54 -0500

    Improve support for Git SSH key with local Jenkins
    The jenkins/start.sh now recognizes option -key <path_to_private_key>
    and uses it to create the SSH Git credentials when starting new local
    Jenkins instance for the first time.
    
    The #137 is fixed by adding missed argument to the jenkins/Dockerfile.
    
    Also the documentation now reflects the additional parameter to start.sh

--- jenkins/start.sh
+++ jenkins/start.sh
@@ -2,8 +2,8 @@
 
 if [[ $# < 3 ]] ; then
     echo "You have to pass three params"
-    echo "1 - git username with access to the forked repos"
-    echo "2 - git password of that user"
+    echo "1 - git username with access to the forked repos or \"-key\" if using private key authentication instead"
+    echo "2 - git password of that user or path to the private SSH key file if \"-key\" option is specified"
     echo "3 - org or your user id where the forked repos lay"
     echo "4 - (for k8s) docker registry organization - you can leave an empty value"
     echo "5 - (for k8s) docker registry username - you can leave an empty value"
@@ -11,10 +11,13 @@ if [[ $# < 3 ]] ; then
     echo "7 - (for k8s) docker registry email - you can leave an empty value"
     echo "8 - (optional) external ip (for example Docker Machine if you're using one)"
     echo "Example: ./start.sh user pass forkedOrg dockerOrg dockerUser dockerPass dockerEmail"
+    echo "Example: ./start.sh -key ~/.ssh/my_key forkedOrg dockerOrg dockerUser dockerPass dockerEmail"
     echo "Example: ./start.sh user pass forkedOrg dockerOrg dockerUser dockerPass dockerEmail 192.168.99.100"
     echo "Example: ./start.sh user pass forkedOrg '' '' '' '' 192.168.99.100"
     exit 0
 fi
+
+GIT_AUTH_MODE="username/password"
 
 export PIPELINE_GIT_USERNAME="${1}"
 export PIPELINE_GIT_PASSWORD="${2}"
@@ -24,8 +27,13 @@ export DOCKER_REGISTRY_USERNAME="${5}"
 export DOCKER_REGISTRY_PASSWORD="${6}"
 export DOCKER_REGISTRY_EMAIL="${7}"
 export EXTERNAL_IP="${8}"
-# uncomment the line below and change it to read it from your local file
-# export PIPELINE_GIT_SSH_KEY="$( cat ~/.ssh/your_key )"
+
+if [ "-key" == ${PIPELINE_GIT_USERNAME} ]; then
+	export PIPELINE_GIT_USERNAME=""
+	export PIPELINE_GIT_SSH_KEY="$(cat ${PIPELINE_GIT_PASSWORD})"
+	export PIPELINE_GIT_PASSWORD=""
+	GIT_AUTH_MODE="private key"
+fi
 
 if [[ -z "${EXTERNAL_IP}" ]]; then
     EXTERNAL_IP=`echo ${DOCKER_HOST} | cut -d ":" -f 2 | cut -d "/" -f 3`
@@ -34,6 +42,7 @@ if [[ -z "${EXTERNAL_IP}" ]]; then
     fi
 fi
 
+echo "Git authentication mode [${GIT_AUTH_MODE}]"
 echo "Forked organization [${FORKED_ORG}]"
 echo "External IP [${EXTERNAL_IP}]"
 
