6bab72478c229bbc7ac94f696e0c791463f5d881
Jan K <vanjan@protonmail.com>
2018-07-06 10:50:56 +0200

    Fix concourse login
    'concourse' is the username according to https://github.com/spring-cloud/spring-cloud-pipelines/blob/master/concourse/docker-compose.yml#L19

--- concourse/login.sh
+++ concourse/login.sh
@@ -2,7 +2,7 @@
 
 ROOT_ADDRESS=${1:-localhost:8080}
 ALIAS=${2:-docker}
-USERNAME=${3:-username}
+USERNAME=${3:-concourse}
 PASSWORD=${4:-changeme}
 
 fly -t "${ALIAS}" login -c http://"${ROOT_ADDRESS}" -u="${USERNAME}" -p="${PASSWORD}"
