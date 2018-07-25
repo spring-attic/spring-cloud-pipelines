3e494515b64d70894932957bb2b4792f8b9f7464
Marcin Grzejszczak <mgrzejszczak@pivotal.io>
2018-07-25 14:11:15 +0200

    Fixing build

--- jenkins/src/main/groovy/org/springframework/cloud/pipelines/common/JobCustomizer.groovy
+++ jenkins/src/main/groovy/org/springframework/cloud/pipelines/common/JobCustomizer.groovy
@@ -8,30 +8,30 @@ import javaposse.jobdsl.dsl.jobs.FreeStyleJob;
  * @author Marcin Grzejszczak
  * @since 1.0.0
  */
-public interface JobCustomizer {
+interface JobCustomizer {
 
 	/**
 	 * Applicable to all jobs in the pipeline
 	 */
-	void customizeAll(FreeStyleJob job);
+	void customizeAll(FreeStyleJob job)
 
 	/**
 	 * Applicable to jobs from the {@code build} phase
 	 */
-	void customizeBuild(FreeStyleJob job);
+	void customizeBuild(FreeStyleJob job)
 
 	/**
 	 * Applicable to jobs from the {@code test} phase
 	 */
-	void customizeTest(FreeStyleJob job);
+	void customizeTest(FreeStyleJob job)
 
 	/**
 	 * Applicable to jobs from the {@code stage} phase
 	 */
-	void customizeStage(FreeStyleJob job);
+	void customizeStage(FreeStyleJob job)
 
 	/**
 	 * Applicable to jobs from the {@code prod} phase
 	 */
-	void customizeProd(FreeStyleJob job);
+	void customizeProd(FreeStyleJob job)
 }
